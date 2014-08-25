878e9c1ca81395e65237a261c560d99307fa2ea2
rupo <rupo@documentaal.nl>
2014-08-25 09:35:21 +0200

    Initial commit of Tim's conversion

---
+++ ConsumeEvent 2010/ConsumeEvent/ThisAddIn.vb
@@ -0,0 +1,56 @@
+﻿Imports System
+Imports System.Windows.Forms
+Imports HasEvent
+Imports Office = Microsoft.Office.Core
+
+Public Class ThisAddIn
+	Implements IObserver
+
+	Private hasEvent As Office.COMAddIn
+
+	Public Sub AfterSave() Implements IObserver.AfterSave  '' Implements IObserver.AfterSave
+		MessageBox.Show("Before save event fired")
+	End Sub
+
+	Private Sub ThisAddIn_Startup(ByVal sender As Object, ByVal e As EventArgs) Handles Me.Startup
+		Dim comAddin As Office.COMAddIn = FindComAddinByName("HasEvent")
+		Dim hd As ISubject = ObtainSubject(comAddin)
+		hd.Listen(Me)
+	End Sub
+
+	Private Function FindComAddinByName(ByVal name As String) As Office.COMAddIn
+		For Each comAddIn As Office.COMAddIn In Application.COMAddIns
+			If (comAddIn.Description = name) Then
+				Return comAddIn
+			End If
+		Next
+		Return Nothing
+	End Function
+
+	''' <summary>
+	''' Obtains a reference to a typed ComAddin project.
+	''' The type is determined in the other ComAddin project.
+	''' </summary>
+	''' <seealso cref="http://blogs.msdn.com/b/andreww/archive/2008/08/13/comaddins-race-condition.aspx"/>
+	''' <param name="comAddin"></param>
+	''' <returns></returns>
+	Private Function ObtainSubject(ByVal comAddin As Office.COMAddIn) As ISubject
+		Dim obj As Object = Nothing
+		Dim tries As Integer = 0
+		' 50 * 100 miliseconds = 5000 milliseconds == 5 seconds
+
+		While ((obj Is Nothing) _
+					AndAlso (tries < 50))
+			obj = comAddin.Object
+			System.Threading.Thread.Sleep(100)
+			tries = (tries + 1)
+
+		End While
+		Dim subject As ISubject = CType(obj, ISubject)
+		Return subject
+	End Function
+
+	Private Sub ThisAddIn_Shutdown(ByVal sender As Object, ByVal e As EventArgs) Handles Me.Shutdown
+
+	End Sub
+End Class
---
+++ HasEvent 2010/HasEvent/ThisAddIn.vb
@@ -0,0 +1,70 @@
+﻿Imports System.Collections.Generic
+Imports System.Runtime.InteropServices
+Imports Microsoft.Office.Interop.Word
+Imports Office = Microsoft.Office.Core
+
+''' The observer pattern is needed to do eventing in COM. That's because
+''' COM does not recognizes delegates and therefore does not recognize native 
+''' .NET events.
+''' El Zorko explains this better in 
+''' http://stackoverflow.com/questions/1985451/exposing-a-net-class-which-has-events-to-com
+''' <seealso cref="http://en.wikipedia.org/wiki/Observer_pattern"/>
+''' <summary>
+''' Third-party interested in the Save event
+''' </summary>
+<ComVisible(True), _
+ Guid("12810C22-3FF2-4fc2-A7FD-7E1034462EB0")> _
+Public Interface IObserver
+
+	' Called by the first party when the first party handled the
+	' Word interop BeforeSave event
+	Sub AfterSave()
+End Interface
+
+''' <summary>
+''' The first party needing to gain first-fire access to the BeforeSave event
+''' </summary>
+<ComVisible(True), _
+ Guid("02810C22-3FF2-4fc2-A7FD-7E1034462EB0")> _
+Public Interface ISubject
+
+	Sub Listen(ByVal observer As IObserver)
+
+	Sub Fire()
+End Interface
+
+<ComVisible(True), _
+ Guid("02810C22-3FF2-4fc2-A7FD-5E1034462EB0"), _
+ ClassInterface(ClassInterfaceType.None)> _
+Partial Public Class ThisAddIn
+	Implements ISubject
+
+	Private _observers As List(Of IObserver) = New List(Of IObserver)
+
+	Public Sub Listen(ByVal observer As IObserver) Implements ISubject.Listen
+		_observers.Add(observer)
+	End Sub
+
+	Private Sub Application_DocumentBeforeSave(ByVal Doc As Document, ByRef SaveAsUI As Boolean, ByRef Cancel As Boolean) Handles Application.DocumentBeforeSave
+		Fire()
+	End Sub
+
+	Private Sub ThisAddIn_Shutdown(ByVal sender As Object, ByVal e As System.EventArgs)
+
+	End Sub
+
+	Public Sub Fire() Implements ISubject.Fire
+		For Each observer As IObserver In _observers
+			observer.AfterSave()
+		Next
+	End Sub
+
+	''' <summary>
+	''' Required (and undocumented on the COMAddin MSDN docs) override.
+	''' </summary>
+	''' <returns></returns>
+	Protected Overrides Function RequestComAddInAutomationService() As Object
+		Return Me
+	End Function
+
+End Class
